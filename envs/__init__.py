92034ef81f40f07c1809b6f8d59b85429203b49c
yiqi <yiqi.feng@tum.de>
2022-10-22 15:59:02 +0200

    temporarily delete import

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,20 +1,20 @@
-from .riemann2d import RiemannConfig3Env, RiemannConfig3HighRes128Env, RiemannConfig3HighRes256Env
-from .implosion import ImplosionEnv, ImplosionHighRes128Env
-# from .implosion_outflow import ImplosionOutFLowEnv, ImplosionOutFlowHighResEnv
-from .rmi import RMIEnv, RMIHighRes128Env, RMIHighRes256Env, RMIHighRes512Env
-from .env_base import AlpacaEnv
+# from .riemann2d import RiemannConfig3Env, RiemannConfig3HighRes128Env, RiemannConfig3HighRes256Env
+# from .implosion import ImplosionEnv, ImplosionHighRes128Env
+# # from .implosion_outflow import ImplosionOutFLowEnv, ImplosionOutFlowHighResEnv
+# from .rmi import RMIEnv, RMIHighRes128Env, RMIHighRes256Env, RMIHighRes512Env
+# from .env_base import AlpacaEnv
 
-__all__ = [
-    "AlpacaEnv",
-    "RiemannConfig3Env",
-    "RiemannConfig3HighRes128Env",
-    "RiemannConfig3HighRes256Env",
-    "ImplosionEnv",
-    "ImplosionHighRes128Env",
-    # "ImplosionOutFLowEnv",
-    # "ImplosionOutFlowHighResEnv",
-    "RMIEnv",
-    "RMIHighRes128Env",
-    "RMIHighRes256Env",
-    "RMIHighRes512Env"
-]
+# __all__ = [
+#     "AlpacaEnv",
+#     "RiemannConfig3Env",
+#     "RiemannConfig3HighRes128Env",
+#     "RiemannConfig3HighRes256Env",
+#     "ImplosionEnv",
+#     "ImplosionHighRes128Env",
+#     # "ImplosionOutFLowEnv",
+#     # "ImplosionOutFlowHighResEnv",
+#     "RMIEnv",
+#     "RMIHighRes128Env",
+#     "RMIHighRes256Env",
+#     "RMIHighRes512Env"
+# ]
