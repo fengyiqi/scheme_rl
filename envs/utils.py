a6413b6a2c24df25f8911635c332aaec14e974a0
yiqi <yiqi.feng@tum.de>
2022-10-22 16:00:41 +0200

    define coefs computation func

--- envs/utils.py
+++ envs/utils.py
@@ -1,3 +1,18 @@
+import pandas as pd
+import numpy as np
+import os
+
 def normalize(value, bounds):
     normalized = (value - bounds[0]) / (bounds[1] - bounds[0])
     return normalized
+
+
+def get_scale_coefs(file: str, end_time: float, timestep_size: float):
+    assert os.path.exists(file), "Scale coefs file not found!"
+    data = pd.read_csv(file)
+    coef = data["ke_reward"] / data["disper_imp"]
+    coef_dict = {}
+    for i, t in enumerate(np.arange(0.01, end_time + 1e-6, timestep_size)):
+        time = format(t, ".3f")
+        coef_dict[time] = coef[i] if coef[i] > 0 else 1
+    return coef_dict
